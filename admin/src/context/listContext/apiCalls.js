3de0e4c45377ed235dc3c4a707818a3682f9c52a
EssienNsikak <nsikak.essienceo@gmail.com>
2021-07-21 20:17:34 +0100

    list context implemented

---
+++ admin/src/context/listContext/apiCalls.js
@@ -0,0 +1,56 @@
+import Axios from 'axios';
+import {
+  createListFailure,
+  createListStart,
+  createListSuccess,
+  deleteListFailure,
+  deleteListStart,
+  deleteListSuccess,
+  getListsFailure,
+  getListsStart,
+  getListsSuccess,
+} from './ListActions';
+
+export const getLists = async (dispatch) => {
+  dispatch(getListsStart());
+  try {
+    const res = await Axios.get('http://localhost:5004/api/list', {
+      headers: {
+        token: 'Bearer ' + JSON.parse(localStorage.getItem('user')).accessToken,
+      },
+    });
+    dispatch(getListsSuccess(res.data));
+  } catch (err) {
+    dispatch(getListsFailure());
+  }
+};
+
+//create
+export const createList = async (list, dispatch) => {
+  dispatch(createListStart());
+  try {
+    const res = await Axios.post('http://localhost:5004/api/list', list, {
+      headers: {
+        token: 'Bearer ' + JSON.parse(localStorage.getItem('user')).accessToken,
+      },
+    });
+    dispatch(createListSuccess(res.data));
+  } catch (err) {
+    dispatch(createListFailure());
+  }
+};
+
+//delete
+export const deleteList = async (id, dispatch) => {
+  dispatch(deleteListStart());
+  try {
+    await Axios.delete('http://localhost:5004/api/list' + id, {
+      headers: {
+        token: 'Bearer ' + JSON.parse(localStorage.getItem('user')).accessToken,
+      },
+    });
+    dispatch(deleteListSuccess(id));
+  } catch (err) {
+    dispatch(deleteListFailure());
+  }
+};
