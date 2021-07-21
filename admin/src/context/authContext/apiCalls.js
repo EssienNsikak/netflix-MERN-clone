6a863fae1606d949add6a3a541a81f12694dcd29
EssienNsikak <nsikak.essienceo@gmail.com>
2021-07-21 20:05:48 +0100

    auth context added

---
+++ admin/src/context/authContext/apiCalls.js
@@ -0,0 +1,12 @@
+import Axios from 'axios';
+import { loginFailure, loginStart, loginSuccess } from './AuthActions';
+
+export const login = async (user, dispatch) => {
+  dispatch(loginStart());
+  try {
+    const res = await Axios.post('http://localhost:5004/api/auth/login', user);
+    res.data.isAdmin && dispatch(loginSuccess(res.data));
+  } catch (err) {
+    dispatch(loginFailure());
+  }
+};
