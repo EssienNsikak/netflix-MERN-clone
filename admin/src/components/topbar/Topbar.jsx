4bec8991bdd0d73cd5e146fc84c21f018cdf8797
EssienNsikak <nsikak.essienceo@gmail.com>
2021-07-24 15:17:24 +0100

    topbar fixed

--- admin/src/components/topbar/Topbar.jsx
+++ admin/src/components/topbar/Topbar.jsx
@@ -1,26 +1,28 @@
-import "./topbar.css";
-import { NotificationsNone, Language, Settings } from "@material-ui/icons";
+import './topbar.css';
+import { NotificationsNone, Language, Settings } from '@material-ui/icons';
+import profilePicture from '../../assets/myPic.jpg';
+import Avatar from '../../assets/default-avatar.jpeg'
 
 export default function Topbar() {
   return (
-    <div className="topbar">
-      <div className="topbarWrapper">
-        <div className="topLeft">
-          <span className="logo">netflix-admin-dashboard</span>
+    <div className='topbar'>
+      <div className='topbarWrapper'>
+        <div className='topLeft'>
+          <span className='logo'>netflix-admin-dashboard</span>
         </div>
-        <div className="topRight">
-          <div className="topbarIconContainer">
+        <div className='topRight'>
+          <div className='topbarIconContainer'>
             <NotificationsNone />
-            <span className="topIconBadge">2</span>
+            <span className='topIconBadge'>2</span>
           </div>
-          <div className="topbarIconContainer">
+          <div className='topbarIconContainer'>
             <Language />
-            <span className="topIconBadge">2</span>
+            <span className='topIconBadge'>2</span>
           </div>
-          <div className="topbarIconContainer">
+          <div className='topbarIconContainer'>
             <Settings />
           </div>
-          <img src="https://images.pexels.com/photos/1526814/pexels-photo-1526814.jpeg?auto=compress&cs=tinysrgb&dpr=2&w=500" alt="" className="topAvatar" />
+          <img src={profilePicture || Avatar} alt ='' className='topAvatar' />
         </div>
       </div>
     </div>
